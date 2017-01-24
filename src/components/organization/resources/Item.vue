53438623547da4ac90a089720a8b597e697376f5
Christian Opitz <christian.opitz@netresearch.de>
2017-01-24 22:53:08 +0100

    Dont show dots when no action available

--- src/components/organization/resources/Item.vue
+++ src/components/organization/resources/Item.vue
@@ -5,7 +5,7 @@
         <div class="md-title">{{item.title}}</div>
         <div class="md-subhead">{{item.description}}</div>
       </md-card-header-text>
-      <md-menu v-if="!trash" md-size="4" md-direction="bottom left">
+      <md-menu v-if="!trash && permissions[type].write && item.creator === auth.user.uid" md-size="4" md-direction="bottom left">
         <md-button class="md-icon-button" md-menu-trigger>
           <md-icon>more_vert</md-icon>
         </md-button>
@@ -22,16 +22,13 @@
           </md-menu-item>
         </md-menu-content>
       </md-menu>
-      <md-button v-else @click="toggleTrash" :title="$t('actions.restore')" class="md-icon-button">
+      <md-button v-else-if="trash" @click="toggleTrash" :title="$t('actions.restore')" class="md-icon-button">
         <md-icon>delete_sweep</md-icon>
       </md-button>
     </md-card-header>
     <md-card-media v-if="item.image">
       <resource-image :image="item.image"></resource-image>
     </md-card-media>
-    <md-card-content>
-      Huhu
-    </md-card-content>
   </md-card>
 </template>
 
