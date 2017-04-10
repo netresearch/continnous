f9f8c5d5e4baee540ae48f07739a3737cfa83e80
Christian Opitz <christian.opitz@netresearch.nr>
2017-04-10 19:43:38 +0200

    Added autolinker for editor text

--- src/md/mdEditor/mdEditorText.vue
+++ src/md/mdEditor/mdEditorText.vue
@@ -1,13 +1,33 @@
 <template>
   <div class="md-editor-text">
-    <div class="ql-editor" v-if="text" v-html="text"></div>
+    <div class="ql-editor" v-if="formatedText" v-html="formatedText"></div>
   </div>
 </template>
 
 <script>
   export default {
     props: {
-      text: String
+      text: String,
+      autolink: {
+        type: [Boolean, Object],
+        default() {
+          return {
+            phone: false,
+            mention: false,
+          };
+        }
+      }
+    },
+    computed: {
+      formatedText() {
+        if (!this.text || !this.autolink) {
+          return this.text;
+        }
+        /* eslint-disable global-require */
+        const Autolinker = require('autolinker');
+
+        return Autolinker.link(this.text, typeof this.autolink === 'object' ? this.autolink : {});
+      }
     }
   };
 </script>
